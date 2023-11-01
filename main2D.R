72ed20f63f004c08b1a2f2174b2d6c59698ba886
funstatpackages <49455474+funstatpackages@users.noreply.github.com>
2023-11-01 11:23:45 -0400

    Update main2D.R

--- main2D.R
+++ main2D.R
@@ -1,10 +1,9 @@
 rm(list = ls())
 
-library(parallel)
-library(Matrix)
-library(pracma)
-library(MGLM)
-library(MPST)
+library(devtools)
+install_github("funstatpackages/MPST")
+library(MPST)  
+library(profvis)
 data(VT.square)
 Triangulation::TriPlot(V, Tr)
 
@@ -18,13 +17,13 @@ uu.grid = rep(u.grid, each = n2.grid)
 vv.grid = rep(v.grid, times = n1.grid)
 Z.grid = as.matrix(cbind(uu.grid, vv.grid))
 
-func = 2; sigma = 1;
+func = 1; sigma = 1;
 pop = dataGenerator2D(Z.grid, V, Tr, func, sigma)
 Y.grid = pop$Y; mu.grid = pop$mu;
 ind.grid = pop$ind.inside;
 
 # Simulation parameters
-n = 50000;
+n = 5000;
 lambda = 10^seq(-6, 6, by = 0.5)
 iter = 1
 Z = matrix(runif(2*n, 0, 1), nrow = n, ncol = 2)
@@ -32,8 +31,11 @@ sam = dataGenerator2D(Z, V, Tr, func, sigma, iter)
 Y = as.vector(sam$Y); Z = as.matrix(sam$Z);
 
 t0.g = proc.time()
-mfit.g = fit.MPST(Y = Y, Z = Z, V = V, Tr = Tr, d = d, r = r, lambda = lambda, nl = 1, method = "G")
+profvis({
+mfit.g = fit.MPST(Y = Y, Z = Z, V = V, Tr = Tr, d = d, r = r, lambda = lambda, method = "G")
+})
 proc.time() - t0.g
+
 t1.g = proc.time()
 mpred.g = predict.MPST(mfit.g, Z.grid)
 proc.time() - t1.g
@@ -41,8 +43,11 @@ mspe.g = mean((Y.grid - mpred.g$Ypred)^2, na.rm = TRUE)
 cat("mse.g =", mfit.g$mse, "and mspe.g =", mspe.g, "\n")
 
 t0.d = proc.time()
+profvis({
 mfit.d = fit.MPST(Y = Y, Z = Z, V = V, Tr = Tr, d = d, r = r, lambda = lambda, nl = 1, method = "D")
+})
 proc.time() - t0.d
+
 t1.d = proc.time()
 mpred.d = predict.MPST(mfit.d, Z.grid)
 proc.time() - t1.d
