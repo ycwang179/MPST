3eb8ef6b6f8d925bcc4dcb97428d11e35c69b0fa
Yuchun Wang <ywang68@gmu.edu>
2024-12-09 13:31:42 -0500

    Create BasisFns2D.h

---
+++ src/BasisFns2D.h
@@ -0,0 +1,10 @@
+#ifndef BASISFNS2D_H
+#define BASISFNS2D_H
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// Declare the mtxcbind function.
+NumericMatrix mtxcbind(NumericMatrix a, NumericMatrix b);
+
+#endif // BASISFNS2D_H
